18d71dca9ad95873d5d41c6afad2f23ee24c14d6
Default Seadas User <seadas-user@localhost>
2022-03-01 16:04:23 -0900

    nothing to report

--- 1D_model/1D_mu_model.py
+++ 1D_model/1D_mu_model.py
@@ -25,46 +25,51 @@ def velocity(x,U,H,W,dx):
         dee = 1e-15 # finite strain rate to prevent infinite viscosity
         # fix later; could modify equations so that dU/dx = 0 if ee=0.
         ee = np.sqrt(np.gradient(U,dx)**2)+dee # second invariant of strain rate
+
         I = ee*d/np.sqrt((0.5*g*(1-rho/rho_w)*H))
         mu = muS + I*(mu0-muS)/(I0+I)
                            
-        # constructing matrix Cx = T to solve for velocity
-        # if gg = 0 > modify matrix C so that each line with gg=0 indicates that dU/dx = 0 
-        T = np.zeros(len(x))
-        T[1:-1] = ((2*H[:-1]-d)*(H[1:]-H[:-1])*dx + 2*mu0/W[:-1]*H[:-1]**2*dx**2)[1:]
-        T[0] = Ut # upstream boundary moves at terminus velocity
-        T[-1] = 0 # strain rate equals zero at downstream boundary
+        # constructing matrix Dx = T to solve for velocity
         
+        T = ((2*H[:-1]-d)*np.diff(H)*dx + 2*mu0/W[:-1]*H[:-1]**2*dx**2)
+        T[0] = Ut # upstream boundary moves at terminus velocity
+        T = np.append(T,(1-d/H[-1])*ee[-1]/mu[-1]) # downsream boundary condition
         
         # create staggered grid, using linear interpolation
-        #xn = np.linspace(x[0]+dx/2, x[-1]+dx/2, len(x)-1 ) # this needs to be checked!
-        xn = np.arange(x[0]+dx/2,x[-1]+dx/2,dx)
+        xn = np.linspace(x[0]+dx/2, x[-1]+dx/2, len(x)-1 )
         nu = np.interp(xn,x,mu/ee*H**2) # create new variable on staggered grid to simplify later
 
+        A = nu[:-1]
+        B = -(nu[:-1]+nu[1:])
+        C = nu[1:]
 
+
+        # use a_left, a, and a_right define the diagonals of D
+        a_left = np.append(A, -1)
+        
         a = np.ones(len(T)) # set to positive one because default is to set strain rate equal to zero
-        a[0] = 1 # specify upstream boundary condition
-        a[1:-1] = -(nu[1:]+nu[0:-1]) # staggered grid
-        a[-1] = 1 # needed for specifying downstream boundary condition
+        a[1:-1] = B
+        a[-1] = 1
+                                   
+        a_right = np.append(0,C)
         
-        a_left = nu
-        a_left[-1] = -1 # needed for specifing downstream boundary condition; this line might be redundant
-                                
-        a_right = nu
-        a_right[0] = 0
+          
+        # print('a: ' + str(len(a)))
+        # print('a_left: ' + str(len(a_left)))
+        # print('a_right: ' + str(len(a_right)))
         
-               
         diagonals = [a_left,a,a_right]
-        C = diags(diagonals,[-1,0,1]).toarray() 
+        D = diags(diagonals,[-1,0,1]).toarray() 
          
-        U_new = np.linalg.solve(C,T) # solve for velocity
+        U_new = np.linalg.solve(D,T) # solve for velocity
         
-        #plt.plot(x,mu)
+        
         
         if (np.abs(U-U_new)*secsYear > 1).any():        
                 U = U_new
         else:
             U = U_new
+            #plt.plot(x,mu)
             break
                
     return(U)
@@ -86,7 +91,7 @@ d = 25 # characteristic iceberg size
 
 x0 = 0
 L = 1e4+x0 # melange length
-dx = 10 # grid spacing
+dx = 100 # grid spacing
 x = np.arange(x0,L+dx,dx) # longitudinal grid
 W = 5000*np.ones(x.shape) # melange width; treated as constant for now
 
@@ -97,7 +102,7 @@ Ut = 5000/secsYear # glacier terminus velocity
 
 
 # max and min effective coefficients of friction, sort of
-mu0 = 0.6
+mu0 = 0.5
 muS = 0.2
 I0 = 10**-7
 
@@ -115,15 +120,16 @@ ax2 = plt.subplot(212)
 ax2.set_xlabel('Longitudinal coordinate [m]')
 ax2.set_ylabel('Thickness [m]')
 
-color_id = np.linspace(0,1,100) 
+n = 1000 # number of time steps
+color_id = np.linspace(0,1,n) 
 
 
-for k in np.arange(0,100):
+for k in np.arange(0,n):
          
     U = velocity(x,U,H,W,dx)
     #plt.plot(x,U*secsYear)
     
-    if k % 10 == 0:
+    if k % 100 == 0:
         ax1.plot(x,U*secsYear,color=plt.cm.viridis(color_id[k]))
         ax2.plot(x,H,color=plt.cm.viridis(color_id[k]))
     
@@ -131,7 +137,7 @@ for k in np.arange(0,100):
     H += (B-np.gradient(H*W*U,x)/W)*dt
     
     
-    #update grid
+    # update grid
     x0 = x[0]+Ut*dt
     xL = x[-1]+U[-1]*dt
 
