Fixed small error with pressure in the local granular fluidity calculation.

figures/fig2-vary_parameters/fig-vary_parameters.py
@@ -20,8 +20,8 @@ import matplotlib
 
 import sys
 
-sys.path.append('/home/jason/projects/glaciome/glaciome1D')
-from glaciome1D import glaciome, basic_figure, plot_basic_figure, constants
+sys.path.append('/hdd/glaciome/models/glaciome1D')
+from glaciome1D import glaciome, constants
 import matplotlib.patheffects as PathEffects
 
 
@@ -47,7 +47,7 @@ constant = constants()
 
 
 #%% 
-run_simulations = 'n'
+run_simulations = 'y'
 
 if run_simulations == 'y':
     
@@ -68,7 +68,7 @@ if run_simulations == 'y':
     
     file_extensions = ['','_varymuS','_varyd','_varyb','_varyA']
 
-    for j in np.arange(2,3):   
+    for j in np.array([1,3,4]):#np.arange(2,3):   
         
         B = -0.6*constant.daysYear
         data = glaciome(n_pts, dt, L, Ut, Uc, Ht, B, X_fjord, W_fjord)

figures/fig4-vary_B/fig-vary_B.py
@@ -20,7 +20,7 @@ import matplotlib
 
 import sys
 
-sys.path.append('/home/jason/projects/glaciome/glaciome1D')
+sys.path.append('/hdd/glaciome/models/glaciome1D')
 from glaciome1D import glaciome, constants
 import matplotlib.patheffects as PathEffects
 
@@ -69,7 +69,7 @@ if run_simulations == 'y':
     data = glaciome(n_pts, dt, L, Ut, Uc, Ht, B[0], X_fjord, W_fjord)
     
 
-    for j in np.arange(2,len(B)):
+    for j in np.arange(0,2):
             
         data.B = B[j]
         data.steadystate(method='lm')
@@ -81,8 +81,6 @@ if run_simulations == 'y':
 
 #%%
 files = sorted(glob.glob('*.pickle'))
-#files = files[10:]
-#files = files[::5]
 
 
 n = len(files)

figures/fig5-varyW/fig_varyW.py
@@ -12,7 +12,7 @@ import matplotlib
 import matplotlib.patheffects as PathEffects
 import sys
 
-sys.path.append('/home/jason/projects/glaciome/glaciome1D')
+sys.path.append('/hdd/glaciome/models/glaciome1D')
 from glaciome1D import glaciome, constants
 
 import os
@@ -40,7 +40,6 @@ constant = constants()
 #%%
 
 run_simulations = 'y'
-
 terminus_width = np.linspace(3000,7000,5,endpoint=True)
 dWdx = np.linspace(-0.04,0.04,5)
 
@@ -48,16 +47,16 @@ if run_simulations == 'y':
     
     shutil.copyfile('../fig1-steady_state_profile/steady-state_Bdot_-0.60.pickle','./steady-state_Wt_4000.0.pickle')
     
-    # n_pts = 51 # number of grid points
-    # L = 1e4 # ice melange length
-    # Ut = 0.6e4 # glacier terminus velocity [m/a]; treated as a constant
-    # Uc = 0.6e4 # glacier calving rate [m/a]; treated as a constant
-    # Ht = 600 # terminus thickness
-    # n = 101 # number of time steps
-    # dt = 0.01# 1/(n_pts-1)/10 # time step [a]; needs to be quite small for this to work
-    # B = -0.6*constant.daysYear
+    n_pts = 51 # number of grid points
+    L = 1e4 # ice melange length
+    Ut = 0.6e4 # glacier terminus velocity [m/a]; treated as a constant
+    Uc = 0.6e4 # glacier calving rate [m/a]; treated as a constant
+    Ht = 600 # terminus thickness
+    n = 101 # number of time steps
+    dt = 0.01# 1/(n_pts-1)/10 # time step [a]; needs to be quite small for this to work
+    B = -0.6*constant.daysYear
     
-    for j in np.array([4]):#np.array([0,2,3,4]):
+    for j in np.arange(0,len(terminus_width)): #np.array([4]):#np.array([0,2,3,4]):
         
         file = open('./steady-state_Wt_4000.0.pickle','rb')
         data = pickle.load(file)
@@ -87,8 +86,9 @@ if run_simulations == 'y':
         file.close()    
     
         # specifying fjord geometry
-        W_fjord = data.Wt + dWdx[j]*data.X_fjord
-       
+        data.Wt = data.W0
+        data.W_fjord = data.Wt + dWdx[j]*data.X_fjord
+        
         print('dW/dx: ' + "{0}".format(dWdx[j]))
         
         data.steadystate(method='lm')

glaciome1D.py
@@ -591,7 +591,8 @@ class glaciome:
         # np.diff(self.U)/np.diff(self.x*self.L)
         
         exx = np.diff(self.U)/(self.dx*self.L)
-        gamma = 1 + exx/(self.gg-exx)
+        #gamma = 1 + exx/(self.gg-exx)
+        gamma = self.gg/(self.gg+exx)
         
         g_loc = constant.secsYear*np.sqrt(self.pressure(H)*gamma/(constant.rho*self.param.d**2*self.param.Hscale))*(1-self.param.muS/mu)/(self.param.b)
         